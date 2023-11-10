29134baf776ce794b5d813b8c189b07f9bafd5cb
Gius3ppe95 <Snak3Ey3s.2023@gmail.com>
2023-11-10 16:51:58 +0100

    commit 3

--- shop.js
+++ shop.js
@@ -1,22 +1,25 @@
 let products = [];
 let editMode = false;
 let productIdToEdit = null;
+let loading = false;
 
 function submitForm() {
   const name = document.getElementById("name").value;
 
   // Aggiungi qui la validazione per gli altri campi se necessario
   if (!name) {
-    alert("Il campo 'Name' è obbligatorio.");
+    showError("Il campo 'Name' è obbligatorio.");
     return;
   }
 
-  showLoadingIndicator(); // Mostra l'indicatore di caricamento
+  showLoadingIndicator();
 
   if (editMode) {
     const productIndex = products.findIndex((product) => product._id === productIdToEdit);
     if (productIndex !== -1) {
       products[productIndex] = { _id: productIdToEdit, name }; // Aggiungi altri campi
+    } else {
+      showError("Il prodotto da modificare non è stato trovato.");
     }
     editMode = false;
   } else {
@@ -27,10 +30,19 @@ function submitForm() {
   renderProductList();
   resetForm();
 
-  hideLoadingIndicator(); // Nascondi l'indicatore di caricamento
+  hideLoadingIndicator();
 }
 
 function resetForm() {
+  const isFormDirty = isFormEdited();
+
+  if (isFormDirty) {
+    const confirmation = window.confirm("Sei sicuro di voler resettare il form? Tutte le modifiche saranno perse.");
+    if (!confirmation) {
+      return;
+    }
+  }
+
   document.getElementById("productForm").reset();
   editMode = false;
   productIdToEdit = null;
@@ -47,7 +59,14 @@ function editProduct(productId) {
 }
 
 function deleteProduct(productId) {
-  if (window.confirm("Sei sicuro di voler eliminare questo prodotto?")) {
+  const productToDelete = products.find((product) => product._id === productId);
+
+  if (!productToDelete) {
+    showError("Il prodotto da eliminare non è stato trovato.");
+    return;
+  }
+
+  if (window.confirm(`Sei sicuro di voler eliminare il prodotto: ${productToDelete.name}?`)) {
     products = products.filter((product) => product._id !== productId);
     renderProductList();
     resetForm();
@@ -85,52 +104,6 @@ function renderProductList() {
 }
 
 function confirmDelete(productId) {
-  if (window.confirm("Sei sicuro di voler eliminare questo prodotto?")) {
-    deleteProduct(productId);
-  }
-}
-
-function showLoadingIndicator() {
-  // Logica per mostrare l'indicatore di caricamento
-}
-
-function hideLoadingIndicator() {
-  // Logica per nascondere l'indicatore di caricamento
-}
-function showError(message) {
-  alert(`Errore: ${message}`);
-}
-
-function submitForm() {
-  const name = document.getElementById("name").value;
-
-  if (!name) {
-    showError("Il campo 'Name' è obbligatorio.");
-    return;
-  }
-
-  showLoadingIndicator();
-
-  if (editMode) {
-    const productIndex = products.findIndex((product) => product._id === productIdToEdit);
-    if (productIndex !== -1) {
-      products[productIndex] = { _id: productIdToEdit, name }; // Aggiungi altri campi
-    } else {
-      showError("Il prodotto da modificare non è stato trovato.");
-    }
-    editMode = false;
-  } else {
-    const newProduct = { _id: Date.now(), name }; // Aggiungi altri campi
-    products.push(newProduct);
-  }
-
-  renderProductList();
-  resetForm();
-
-  hideLoadingIndicator();
-}
-
-function deleteProduct(productId) {
   const productToDelete = products.find((product) => product._id === productId);
 
   if (!productToDelete) {
@@ -138,11 +111,57 @@ function deleteProduct(productId) {
     return;
   }
 
-  if (window.confirm("Sei sicuro di voler eliminare questo prodotto?")) {
-    products = products.filter((product) => product._id !== productId);
-    renderProductList();
-    resetForm();
+  const confirmation = window.confirm(`Sei sicuro di voler eliminare il prodotto: ${productToDelete.name}?`);
+  if (confirmation) {
+    deleteProduct(productId);
   }
 }
 
-// Aggiungi gestione degli errori anche ad altre funzioni se necessario
+function showProductDetails(productId) {
+  const product = products.find((product) => product._id === productId);
+
+  if (!product) {
+    showError("Dettagli del prodotto non disponibili.");
+    return;
+  }
+
+  // Aggiungi qui la logica per visualizzare i dettagli del prodotto (se necessario)
+}
+
+function isFormEdited() {
+  const nameInput = document.getElementById("name");
+  // Aggiungi qui la verifica per altri campi del form
+  return nameInput.value.trim() !== "";
+}
+
+function showLoadingIndicator() {
+  loading = true;
+  renderLoadingIndicator();
+}
+
+function hideLoadingIndicator() {
+  loading = false;
+  renderLoadingIndicator();
+}
+
+function renderLoadingIndicator() {
+  const container = document.querySelector(".container");
+  const loadingIndicator = document.getElementById("loadingIndicator");
+
+  if (loading) {
+    if (!loadingIndicator) {
+      const indicator = document.createElement("div");
+      indicator.id = "loadingIndicator";
+      indicator.textContent = "Caricamento in corso...";
+      container.appendChild(indicator);
+    }
+  } else {
+    if (loadingIndicator) {
+      container.removeChild(loadingIndicator);
+    }
+  }
+}
+
+function showError(message) {
+  alert(`Errore: ${message}`);
+}
